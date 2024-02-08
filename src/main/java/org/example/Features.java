71e4f40682bb70b0f26e691b9a25b685f0aee90a
José Antonio <jeugfer600@g.educaand.es>
2024-02-08 08:50:29 +0100

    suma añadida

--- src/main/java/org/example/Features.java
+++ src/main/java/org/example/Features.java
@@ -4,4 +4,13 @@ package org.example;
  * This class have all the methods for arthmetic operations
  */
 public class Features {
+    /**
+     * Method to perform adds
+     * @param a operando 1
+     * @param b operando 2
+     * @return the result of the add operation
+     */
+    public float suma(float a, float b){
+        return a + b;
+    }
 }
